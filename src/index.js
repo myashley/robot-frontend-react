96fd736634af67a82f0dbf6796dde8f0fba6255f
Ashley <36495929+myashley@users.noreply.github.com>
2018-06-14 16:49:08 +1000

    Update async func to promise

---
+++ src/index.js
@@ -0,0 +1,70 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+
+const form = document.querySelector('form')
+
+// 3. add a submit event listener to form 
+form.addEventListener('submit', submitRobot)
+
+
+// 4. submitBook or submitForm
+function submitRobot(e){
+    e.preventDefault()
+    const form = e.target.elements
+    const robotName = form.robotName.value
+    const robotType = form.robotType.value
+    const bodyColour = form.bodyColour.value
+    const faceColour = form.faceColour.value
+    const highlightColour = form.highlightColour.value
+    const personality = form.personality.value
+    const message = form.message.value
+    
+    const robot = {
+        robotName, 
+        robotType,
+        bodyColour,
+        faceColour,
+        highlightColour,
+        personality,
+        message
+    }
+
+    // post robot
+    postRobot(robot)
+        .then(robot => {
+            
+            const newRobot = robot.json()
+            console.log(newRobot)
+            // tip: reset form if successfull
+            e.target.reset()
+            // console.log(newRobot)
+            // return createRobotItem(robot)
+        })
+        // .then(newRobot => {     
+        //     console.log(newRobot)
+            
+        // })
+        // .then(el => addRobotToList(el))
+        .catch(err => console.error(err))
+    
+
+}
+
+
+
+// 5. postRobot async function
+function postRobot(robot){
+    const url = 'http://localhost:3000/robots'
+    const options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(robot)
+    }
+    const response = fetch(url, options)
+    return response
+    
+    // const newRobot = response.json()
+    // console.log(newRobot)
+    // return newRobot
+    
+}
